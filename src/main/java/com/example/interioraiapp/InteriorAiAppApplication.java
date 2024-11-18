810af97b24b50b31553a8d5e757ec46306696caa
ATsonev <alexconev1@gmail.com>
2024-11-18 17:33:17 +0200

    Initial commit

---
+++ src/main/java/com/example/interioraiapp/InteriorAiAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.interioraiapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InteriorAiAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InteriorAiAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/interioraiapp/InteriorAiAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.interioraiapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InteriorAiAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
